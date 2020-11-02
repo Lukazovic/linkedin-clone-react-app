85891992dfcb78a3a80033306760bbe5c23bd0c5
Lukazovic <lucas10.vieira.lv@gmail.com>
2020-11-01 22:47:55 -0300

    Feature: Add LoadingProfilePanel to the LeftColumn

--- src/components/LeftColumn/index.tsx
+++ src/components/LeftColumn/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 
 import ProfilePanel from './ProfilePanel';
 import HashtagPanel from './HashtagPanel';
+import LoadingProfilePanel from '../Shimmer/LoadingProfilePanel';
 
 import { Container } from './styles';
 
-const LeftColumn: React.FC = () => {
+const LeftColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="left-column">
-      <ProfilePanel />
-      <HashtagPanel />
+      {isLoading ? (
+        <LoadingProfilePanel />
+      ) : (
+        <>
+          <ProfilePanel />
+          <HashtagPanel />
+        </>
+      )}
     </Container>
   );
 };
